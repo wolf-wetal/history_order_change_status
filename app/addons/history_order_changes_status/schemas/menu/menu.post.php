962fe53f6cb550348db340be486071567be0350f
wolf-wetal <52989571+wolf-wetal@users.noreply.github.com>
2021-02-12 10:45:24 +0600

    Update menu.post.php

--- app/addons/history_order_changes_status/schemas/menu/menu.post.php
+++ app/addons/history_order_changes_status/schemas/menu/menu.post.php
@@ -1,16 +1,4 @@
 <?php
-/***************************************************************************
-*                                                                          *
-*   (c) 2004 Vladimir V. Kalynyak, Alexey V. Vinokurov, Ilya M. Shalnev    *
-*                                                                          *
-* This  is  commercial  software,  only  users  who have purchased a valid *
-* license  and  accept  to the terms of the  License Agreement can install *
-* and use this program.                                                    *
-*                                                                          *
-****************************************************************************
-* PLEASE READ THE FULL TEXT  OF THE SOFTWARE  LICENSE   AGREEMENT  IN  THE *
-* "copyright.txt" FILE PROVIDED WITH THIS DISTRIBUTION PACKAGE.            *
-****************************************************************************/
 
 $schema['central']['orders']['items']['history_order_changes_status'] = array(
     'attrs' => array(
