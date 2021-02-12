25727649d84ee374fad8468a13de8398536d0d30
wolf-wetal <52989571+wolf-wetal@users.noreply.github.com>
2021-02-12 10:42:11 +0600

    Update history_order_changes_status.php

--- app/addons/history_order_changes_status/controllers/backend/history_order_changes_status.php
+++ app/addons/history_order_changes_status/controllers/backend/history_order_changes_status.php
@@ -8,11 +8,9 @@ if ($mode == 'manage') {
         $_REQUEST
     );
 
-    $params['company_id'] = Registry::get('runtime.company_id');
-
     list($history_order_changes_status, $search) = fn_get_history_order_changes_status($params, DESCR_SL);
     $order_statuses = fn_get_statuses(STATUSES_ORDER);
-    $responsibles = fn_call_requests_get_responsibles();
+    $responsibles = fn_history_order_changes_status_get_responsibles();
 
     Tygh::$app['view']
         ->assign('history_order_changes_status', $history_order_changes_status)
